9384838da785300121831739b154941d810cea0b
kenchan13 <kenchan13@gmail.com>
2017-04-14 18:37:04 +0800

    Initial Commit

---
+++ GoChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoChat
+//
+//  Created by Ken Chan on 2017/4/14.
+//  Copyright © 2017年 KenChan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
